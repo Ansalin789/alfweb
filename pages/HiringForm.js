a5e6390f9cd95081fe2260bfdbc8fecfecec5b3c
GomathiKanagaraj <gomathi2641999@gmail.com>
2025-08-05 15:51:33 +0530

    fail popup router

--- pages/HiringForm.js
+++ pages/HiringForm.js
@@ -688,7 +688,7 @@ const HiringForm = ({ onClose }) => {
               <button
                 onClick={() => {
                   setFailedMessage("");
-                  router.push("/");
+                  // router.push("/");
                 }}
                 className="bg-red-600 text-white px-6 py-2 rounded-lg hover:bg-red-700 transition"
               >
